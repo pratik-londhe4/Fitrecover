355b86ab728d58e153ac4c3821d004018cd3f655
pratik-londhe4 <pratik.londhe@rtcamp.com>
2024-10-02 19:25:39 +0530

    Update icon for the info button

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -28,7 +28,7 @@ export default function TabLayout() {
         options={{
           title: 'Info',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'information' : 'information-outline'} color={color} />
           ),
         }}
       />
